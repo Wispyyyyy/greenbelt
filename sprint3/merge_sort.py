d982894736875fc8b0edd88b5f51fa91bb507f54
Peter McMichael <peterjmcmichael1@gmail.com>
2026-02-12 18:18:06 -0800

    merge sort algorithgnm

--- sprint3/merge_sort.py
+++ sprint3/merge_sort.py
@@ -1,20 +1,19 @@
-list = [100, 24, 16, 19, 54, 2, 191, 191, 1, -5, 500, 987, 0]
 
-length = len(list - 1)
+
 # merge sort
 def merge(arr, l, m, r):
-    n1 = m - l + 1
-    n2 = r - m
+    numOfLeftElements = m - l + 1
+    numOfRightElements = r - m
 #comment
-    L = [0] * (n1)
-    R = [0] * (n2)
+    leftArray = [0] * numOfLeftElements
+    rightArray = [0] * numOfRightElements
 
 
-    for i in range(0, n1):
-        L[i] = arr[l + i]
+    for i in range(numOfLeftElements):
+        leftArray[i] = arr[l + i]
 
-    for j in range(0, n2):
-        R[j] = arr[m+1+i]
+    for j in range(numOfRightElements):
+        rightArray[j] = arr[m+1+j]
 
 
     i = 0
@@ -22,6 +21,46 @@ def merge(arr, l, m, r):
     k = l
 
 
+    while i < numOfLeftElements and j < numOfRightElements:
+        if leftArray[i] <= rightArray[j]:
+            arr[k] = leftArray[i]
+            i+=1
+        else:
+            arr[k] = rightArray[j]
+            j+=1
+        k+=1
+    while i < numOfLeftElements:
+        arr[k] = leftArray[i]
+        i+=1
+        k+=1
+
+    while j < numOfRightElements:
+        arr[k] = rightArray[j]
+        j+=1
+        k+=1
+
+def mergeSort(arr, l, r):
+    if l < r:
+        m = l + (r - l) // 2
+
+        mergeSort(arr, l, m)
+        mergeSort(arr, m+1, r)
+
+        merge(arr, l, m, r)
 
 
+
+arr = [100, 24, 16, 19, 54, 2, 191, 191, 1, -5, 500, 987, 0]
+
+n = len(arr)
+
+print("Given array is")
+for i in range(n):
+    print("%d" % arr[i], end = " ")
+
+mergeSort(arr, 0, n - 1)
+
+print("\n\nSorted array is ")
+for i in range(n):
+    print("%d" % arr[i], end = " ")
 
